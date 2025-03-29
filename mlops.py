93be809843749a1ca45505d1412b1dd36b6aec01
jhanvikh <jhanvikhandelwal2024@gmail.com>
2025-03-29 12:05:11 +0530

    dta
    data

---
+++ mlops.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Example dataset
+data = pd.DataFrame({
+    'A': [1, 2, np.nan, 4, 5],
+    'B': [5, np.nan, 3, 2, 1]
+})
+
+# Filling missing values
+filled_data = fill_missing_values(data)
+print(filled_data)
+
+# Standardizing data
+standardized_data = standardize_data(data)
+print(standardized_data)
